b01095d2f792344a7e07ce8e48d5fc3058b15854
Anhtusz <99302082+Anhtusz@users.noreply.github.com>
2024-05-15 14:53:53 +0700

    okey

--- WEBFENHOM8/src/Components/AdminComponent/AdminDashboard/ExamComponent/DetailComponent/Details.js
+++ WEBFENHOM8/src/Components/AdminComponent/AdminDashboard/ExamComponent/DetailComponent/Details.js
@@ -8,12 +8,12 @@
 
    import baseUrl from "../../../../baseUrl";
 
-    
-    function Details(){
-        
-        const {id} = useParams();
 
-        const [exam  , setExam] = useState({
+   function Details(){
+
+      const {id} = useParams();
+
+      const [exam  , setExam] = useState({
             name:"",
             desc:"",
             date: "",
@@ -22,83 +22,83 @@
             passMarks:"",
             level:"",
             totalTime: ""
-        });
+      });
 
-        useEffect(() => {
-          
-             async function getExamDetails(){
-                const value = await axios.get(`${baseUrl}/exam/${id}`);
-                setExam(value.data);
-                console.log(value.data);
-             }
-             getExamDetails();
-        },[id])
+      useEffect(() => {
+         
+            async function getExamDetails(){
+               const value = await axios.get(`${baseUrl}/exam/${id}`);
+               setExam(value.data);
+               console.log(value.data);
+            }
+            getExamDetails();
+      },[id])
 
    // -------------------------Go back function---------------------------------------
-     
+   
       let history = useHistory();
       const formatTime = (time) => {
          const minutes = Math.floor(time / 60);
          const seconds = time % 60;
          return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-     };
+   };
       function handleGoBack(){
-          history.push("/AdminDashboard/Exam");
-      }
+         history.push("/AdminDashboard/Exam");
+   }
 
 
-        return (
+      return (
             <>
-                <div id={style.displayHeadingBox}> 
+               <div id={style.displayHeadingBox}> 
                      <h2>Exam Details</h2>     
-                 </div>
+               </div>
 
-                 <div id={style.tableBox}>
+               <div id={style.tableBox}>
                      <table >
-                         <thead >
-                              <tr>
-                                <th id={style.center}>Exam Name</th>
-                                <td id={style.center}> {exam.name.name} </td>
-                             </tr>
+                        <thead >
+                           <tr>
+                              <th id={style.center}>Exam Name</th>
+                              <td id={style.center}> {exam.name.name} </td>
+                           </tr>
+
+                           <tr>
+                              <th id={style.center}>Exam Description</th>
+                              <td id={style.center}> {exam.desc} </td>
+                           </tr>
+
+                           <tr>
+                                 <th id={style.center}>Exam Creation Date</th>
+                                 <td id={style.center}> {exam.date} </td>
+                           </tr>
 
                               <tr>
-                                <th id={style.center}>Exam Description</th>
-                                <td id={style.center}> {exam.desc} </td>
+                                 <th id={style.center}>Exam TotalTime</th>
+                                 <td id={style.center}> {formatTime(exam.totalTime)} </td>
                               </tr>
 
-                               <tr>
-                                  <th id={style.center}>Exam Creation Date</th>
-                                  <td id={style.center}> {exam.date} </td>
-                               </tr>
+                              <tr>
+                                 <th id={style.center}>Exam TotalQuestion</th>
+                                 <td id={style.center}> {exam.totalQuestion} </td>
+                              </tr>
 
-                               <tr>
-                                  <th id={style.center}>Exam TotalTime</th>
-                                  <td id={style.center}> {formatTime(exam.totalTime)} </td>
-                               </tr>
+                              <tr>
+                                 <th id={style.center}>Exam PassMarks</th>
+                                 <td id={style.center}> {exam.passMarks} </td>
+                              </tr>
 
-                               <tr>
-                                  <th id={style.center}>Exam TotalQuestion</th>
-                                  <td id={style.center}> {exam.totalQuestion} </td>
-                               </tr>
-
-                               <tr>
-                                  <th id={style.center}>Exam PassMarks</th>
-                                  <td id={style.center}> {exam.passMarks} </td>
-                               </tr>
-
-                               <tr>
-                                  <th id={style.center}>Exam Level</th>
-                                  <td id={style.center}> {exam.level} </td>
-                               </tr>
-                            </thead>
-                         </table>
+                              <tr>
+                                 <th id={style.center}>Exam Level</th>
+                                 <td id={style.center}> {exam.level} </td>
+                              </tr>
+                           </thead>
+                        </table>
                      </div>
 
-                    <div id={style.addSubjectBox}>
-                       <button onClick={handleGoBack}>Go Back</button>
-                   </div>
+                  <div id={style.addSubjectBox}>
+                     <button onClick={handleGoBack}>Go Back</button>
+                  </div>
             </>
-        );
-    }
+      );
+   }
 
     export default Details;
